11992250c0ec8076471c4c11407ca2f13b6a1fd9
fuzziecoder <ramvj2005@gmail.com>
2026-01-12 11:53:00 +0530

    fixing build errors

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,9 @@
 export { Dashboard } from './Dashboard';
-export { Pipelines as PipelinesPage } from './Pipelines';
-export { Executions as ExecutionsPage } from './Executions';
-export { AIInsights as AIInsightsPage } from './AIInsights';
-export { Logs as LogsPage } from './Logs';
-export { Alerts as AlertsPage } from './Alerts';
+export { PipelinesPage } from './Pipelines';
+export { ExecutionsPage } from './Executions';
+export { AIInsightsPage } from './AIInsights';
+export { LogsPage } from './Logs';
+export { AlertsPage } from './Alerts';
 export { Settings } from './Settings';
 export { Login } from './Login';
 export { Signup } from './Signup';
